4a7c0b202f3eac72dfc45442e606e152e433d02d
sbj <sbahnj@gmail.com>
2022-10-11 16:56:10 -0700

    a5

---
+++ public/labs/a5/index.js
@@ -0,0 +1,16 @@
+/* alert('Hello World!'); */
+
+console.log('Hello World!');
+
+console.log('Variables and Constants');
+global1 = 10;
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = global1
+    + functionScoped
+    - blockScoped;
+
+console.log(global1)
+console.log(functionScoped)
+console.log(blockScoped)
+console.log(constant1)
