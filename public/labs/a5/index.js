3e933df42615c142248983d44125a7cf378c49fd
sbj <sbahnj@gmail.com>
2022-10-11 18:39:05 -0700

    a5

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -117,3 +117,70 @@ const length1 = numberArray1.length;
 const index1 = numberArray1.indexOf(3);
 console.log(length1)
 console.log(index1)
+
+console.log('Add and remove data from arrays')
+// adding new items
+numberArray1.push(6);
+stringArray1.push('string3');
+
+// remove 1 item starting on 3rd spot
+numberArray1.splice(2, 1);
+stringArray1.splice(1, 1);
+console.log(numberArray1);
+console.log(stringArray1);
+
+console.log('For loops')
+for (let i=0; i<stringArray1.length; i++) {
+    const string1 = stringArray1[i];
+    console.log(string1);
+}
+
+console.log('Map function')
+const squares = numberArray1.map(square);
+const cubes = numberArray1.map(a => a * a * a);
+console.log(squares)
+console.log(cubes)
+
+console.log('Find function')
+const four = numberArray1.find(a => a === 4);
+const string3 = stringArray1.find(a => a === 'string3');
+console.log(four)
+console.log(string3)
+
+console.log('Find index')
+const fourIndex = numberArray1
+    .findIndex(a => a === 4);
+const string3Index = stringArray1
+    .findIndex(a => a === 'string3');
+
+console.log(fourIndex)
+console.log(string3Index)
+
+console.log('Filter function')
+const numbersGreaterThan2 = numberArray1
+    .filter(a => a > 2);
+const evenNumbers = numberArray1
+    .filter(a => a % 2 === 0);
+const oddNumbers = numberArray1
+    .filter(a => a % 2 !== 0);
+
+console.log(numbersGreaterThan2)
+console.log(evenNumbers)
+console.log(oddNumbers)
+
+console.log('Template strings')
+const five = 2 + 3;
+const result1 = "2 + 3 = " + five;
+console.log(result1);
+
+const result2 = `2 + 3 = ${2 + 3}`;
+console.log(result2);
+
+const username = "alice";
+const greeting1 = `Welcome home ${username}`;
+console.log(greeting1);
+
+LoggedIn = false;
+const greeting2 = `Logged in: ${LoggedIn ? "Yes" : "No"}`;
+console.log(greeting2)
+
