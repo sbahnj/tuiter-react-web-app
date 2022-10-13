b5f81fe6387089203baf034821e9e43ba8b5f414
sbj <sbahnj@gmail.com>
2022-10-12 19:08:54 -0700

    tuit

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -6,9 +6,30 @@ const NavigationSidebar = () => {
        
        <!-- continue rest of list, e.g.,
             Home, Explore, Notifications,  Messages, etc. -->
-       <i class="fab fa-twitter"></i></a>
+       <a class="list-group-item" href="/">
+       <i class="fab fa-home"></i>Home</a>
         
+        <a class="list-group-item" href="/">
+       <i class="fab fa-hashtag"></i>Explore</a>
         
+        <a class="list-group-item" href="/">
+       <i class="fab fa-bell"></i>Notifications</a>
+            
+            <a class="list-group-item" href="/">
+       <i class="fab fa-envelope"></i>Messages</a>
+       
+       <a class="list-group-item" href="/">
+       <i class="fab fa-bookmark"></i>Bookmarks</a>
+       
+       <a class="list-group-item" href="/">
+       <i class="fab fa-list"></i>Lists</a>
+       
+       <a class="list-group-item" href="/">
+       <i class="fab fa-user"></i>Profile</a>
+       
+       <a class="list-group-item" href="/">
+       <i class="fab fa-circle"></i>More</a>
+            
             
    </div>
    <div class="d-grid mt-2">
