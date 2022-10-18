a9113da92f66a85412c2e1898e8069d6e19f2ce0
sbj <sbahnj@gmail.com>
2022-10-18 16:08:16 -0700

    a6

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,7 +1,14 @@
+import Nav from "../../nav";
 function HelloWorld() {
-    return(
-        <h1>Hello World!</h1>
+    return (
+        <div>
+            <Nav/>
+            <h1>Hello World!</h1>
+        </div>
     );
-};
+}
 export default HelloWorld;
+
+
+
 
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,9 +1,14 @@
+import Nav from "../nav";
 function Tuiter() {
-    return(
+    return (
         <div>
+            <Nav/>
             <h1>Tuiter</h1>
         </div>
-    );
+    )
 }
 export default Tuiter
+
+
+
 
