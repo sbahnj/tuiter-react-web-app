8aa5f144eb73dacbf0461ca686ca3ba0bb45c1b8
sbj <sbahnj@gmail.com>
2022-10-18 18:58:41 -0700

    a6

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,17 +1,23 @@
-import {Link}
-    from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+    from "./conditional-output";
+
+
 
 function Assignment6() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
             <h1>Assignment 6</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
         </div>
-    )
+    );
 }
 export default Assignment6;
+
+
 
 
 
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,10 +1,13 @@
 import Assignment6 from "./a6";
 import Nav from "../nav";
+import Todos from "../../public/labs/a5/todos/todos";
+
 function Labs() {
     return (
         <div>
             <Nav/>
             <Assignment6/>
+            <Todos/>
         </div>
     );
 }
