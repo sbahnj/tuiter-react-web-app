92a535b4638d279862da4a093d8f594e5f6dee89
sbj <sbahnj@gmail.com>
2022-10-22 15:41:15 -0700

    tuiter

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import PostSummaryList from "../post-summary-list";
 import "../../index.css";
 
-/*import FullPostList from "../full-post-list";
-import NavigationSidebar from "../navigation-sidebar"*/
+
 
 
 const HomeComponent = () => {
